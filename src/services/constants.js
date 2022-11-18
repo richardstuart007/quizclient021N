bc868fb9c062648e3bfc92cd52ab854dd79a4634
Richard <richardstuart007@hotmail.com>
2022-11-18 16:24:02 +1300

    ports

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,20 +1,13 @@
-//
-//  Browser Port (9103) ==> Server REMOTE server
-//
+//------------------------------------------------------------------------
+//  Remote - Production
+//------------------------------------------------------------------------
 exports.REMOTE_CLIENT = 'REMOTE:9103'
 exports.REMOTE_SERVER = 'REMOTE:Netlify/9101'
 exports.REMOTE_DATABASE = 'REMOTE:Elephant/21'
 exports.REMOTE_SERVERURL = 'https://quizserver021n.netlify.app'
-//
-//  9103 - Local Client --> Remote Server --> Remote Database
-//
-exports.LOC_REMOTE_REMOTE_CLIENT = 'LOCAL:9103'
-//
-//  9113 - Local Client --> Local Server --> Remote Database
-//
-exports.LOC_LOC_REMOTE_CLIENT = 'LOCAL:9113'
-exports.LOC_LOC_REMOTE_SERVER = 'LOCAL:9101/21'
-exports.LOC_LOC_REMOTE_SERVERURL = 'http://localhost:9101'
+//------------------------------------------------------------------------
+//  Local - Testing
+//------------------------------------------------------------------------
 //
 //  8103 - Local Client --> Local Server --> Local Database
 //
@@ -23,15 +16,25 @@ exports.LOC_LOC_LOC_SERVER = 'LOCAL:8101'
 exports.LOC_LOC_LOC_DATABASE = 'LOCAL'
 exports.LOC_LOC_LOC_SERVERURL = 'http://localhost:8101'
 //
-//  Server details
+//  9113 - Local Client --> Local Server --> Remote Database
 //
+exports.LOC_LOC_REMOTE_CLIENT = 'LOCAL:9113'
+exports.LOC_LOC_REMOTE_SERVER = 'LOCAL:9101'
+exports.LOC_LOC_REMOTE_SERVERURL = 'http://localhost:9101'
+//
+//  9103 - Local Client --> Remote Server --> Remote Database
+//
+exports.LOC_REMOTE_REMOTE_CLIENT = 'LOCAL:9103'
+//------------------------------------------------------------------------
+//  Server details
+//------------------------------------------------------------------------
 exports.URL_REGISTER = '/QuizRegister'
 exports.URL_SIGNIN = '/QuizSignin'
 exports.URL_PROFILE = '/QuizProfile/:id'
 exports.URL_TABLES = '/QuizTables'
-//
+//------------------------------------------------------------------------
 //  Other Parameters
-//
+//------------------------------------------------------------------------
 exports.MAX_QUESTIONS_SELECT = 50
 exports.WAIT = 100
 exports.WAIT_MAX_TRY = 20
